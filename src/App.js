fc22dfb2f451f5c68ab6e751a7c2fc48038d1efc
avesk <averykushner@gmail.com>
2019-09-27 17:23:25 -0700

    added linting, drawer items are not working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import Dashboard from "aero-ui/Dashboard";
-import Link from "@material-ui/core/Link";
-import Typography from "@material-ui/core/Typography";
+import React from 'react'
+import Dashboard from 'aero-ui/Dashboard'
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
 
 const UvicAero = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"UVic Aero"}
-      <Link color="inherit" href="https://uvicaero.com/" target="_blank">
-        UVic Aero
-      </Link>
-      {" team."}
-    </Typography>
-  );
-};
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'UVic Aero'}
+            <Link color="inherit" href="https://uvicaero.com/" target="_blank">
+                UVic Aero
+            </Link>
+            {' team.'}
+        </Typography>
+    )
+}
 
-const App = () => <Dashboard>{UvicAero()}</Dashboard>;
+const App = () => <Dashboard>{UvicAero()}</Dashboard>
 
-export default App;
+export default App
--- src/aero-ui/Title.js
+++ src/aero-ui/Title.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
 
 export default function Title(props) {
-  return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
-      {props.children}
-    </Typography>
-  );
+    const { children } = props
+    return (
+        <Typography component="h2" variant="h6" color="primary" gutterBottom>
+            {children}
+        </Typography>
+    )
 }
 
 Title.propTypes = {
-  children: PropTypes.node,
-};
+    children: PropTypes.node.isRequired,
+}

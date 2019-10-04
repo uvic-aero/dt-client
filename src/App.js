e580c085f2e8451b11b2fcb8575bfc8a52a46c36
avesk <averykushner@gmail.com>
2019-10-04 12:00:33 -0700

    testing prettier

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ const UvicAero = () => {
             <Link color="inherit" href="https://uvicaero.com/" target="_blank">
                 UVic Aero
             </Link>
-
             {' teams.'}
         </Typography>
     )

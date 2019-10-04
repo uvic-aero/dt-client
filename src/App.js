25f299c44f5304fa06d4f9ba45eb276b3d5c0f58
avesk <averykushner@gmail.com>
2019-10-03 18:11:58 -0700

    making prettier complain??

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const UvicAero = () => {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'UVic Aero'}
+
             <Link color="inherit" href="https://uvicaero.com/" target="_blank">
                 UVic Aero
-                
             </Link>
             {' teams.'}
         </Typography>

a5b84a8610edf4f414bc319b6278218fa00c9480
SirDuck145 <matthiasharden@gmail.com>
2019-10-17 13:39:42 -0700

    Fix formatting

--- src/ImageView.js
+++ src/ImageView.js
@@ -1,5 +1,6 @@
-import React from 'react'
-
-//Parameter is recieved from the ground vehicle
-const ImageView = (image) => "ImageView Component";
+// Parameter from the ground vehicle
+const ImageView = image => {
+    // Must use image to create a panel
+    return `ImageView Component${image}`
+}
 export default ImageView

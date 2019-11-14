83c6f8a15855418bce97ee2e69481a6cf975d677
VarsyG <Varsenik1999@gmail.com>
2019-11-14 10:54:50 -0800

    adding a semicolon rule

--- src/TestImageView.js
+++ src/TestImageView.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const TestImageView = () => {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             Image View
         </Typography>
-    )
-}
+    );
+};
 
-export default TestImageView
+export default TestImageView;
